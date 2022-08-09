4f80a63e862bc45edc70ba20351cc06265aa76fe
Jacó Apolinário <jacoapollinario@gmail.com>
2022-08-09 19:20:06 -0300

    fix transfer

--- src/modules/statements/useCases/createTransfer/CreateTransferUseCase.ts
+++ src/modules/statements/useCases/createTransfer/CreateTransferUseCase.ts
@@ -44,7 +44,7 @@ export class CreateTransferUseCase {
       user_id: sender_id,
       amount,
       description,
-      type: OperationType.TRANSFER
+      type: OperationType.WITHDRAW
     })
 
     const transferOperation = await this.statementsRepository.create({
